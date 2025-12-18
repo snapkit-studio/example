e9967ef2cbcd21e446a3256f6d448abfdb280a25
kimjh <kjh05505@gmail.com>
2025-12-18 21:40:18 +0900

    add: iOS Example

---
+++ swift/Sources/Snapkit_Eaxmple_iOS/WorkSpace.swift
@@ -0,0 +1,16 @@
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "Snapkit_Eaxmple_iOS",
+    projects: [
+        "Snapkit_Eaxmple_iOS",
+        "Coordinator",
+        "HomeDomain",
+        "HomeFeature",
+        "DetailDomain",
+        "DetailFeature",
+        "Design",
+        "Data",
+        "RxSupport"
+    ]
+)
